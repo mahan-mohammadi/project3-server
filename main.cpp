14e68d62d84fd1865e965fb04a307b6068d16e10
mahan <mahan.mohammadi.a@gmail.com>
2025-06-05 18:30:03 +0330

    the prototype

---
+++ main.cpp
@@ -0,0 +1,42 @@
+// C++ program to show the example of server application in
+// socket programming
+#include <cstring>
+#include <string>
+#include <iostream>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+using namespace std;
+
+int main()
+{
+
+
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(8080);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+
+    bind(serverSocket, (struct sockaddr*)&serverAddress,
+         sizeof(serverAddress));
+
+    listen(serverSocket, 5);
+
+    int clientSocket
+        = accept(serverSocket, nullptr, nullptr);
+
+    char buffer[1024] = { 0 };
+    recv(clientSocket, buffer, sizeof(buffer), 0);
+    cout << "Message from client: " << buffer
+              << endl;
+
+    close(serverSocket);
+
+    return 0;
+}
+
+
